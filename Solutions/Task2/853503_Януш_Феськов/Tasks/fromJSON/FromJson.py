59c506c5bcc99b1514bcd5d17baf272fc2b3d1c6
JanushFM <48549833+JanushFM@users.noreply.github.com>
2020-03-27 10:35:17 +0300

    Forgot to mention about copy-pasting

--- Solutions/Task2/853503_Януш_Феськов/Tasks/fromJSON/FromJson.py
+++ Solutions/Task2/853503_Януш_Феськов/Tasks/fromJSON/FromJson.py
@@ -1,3 +1,7 @@
+#This task wask almost fully copy-pasted https://github.com/eatonphil/pj but with:
+#Some performance improvements
+#Fixed error with null.
+
 from Tasks.fromJSON.lexer import lex
 from Tasks.fromJSON.parser import parse
 
